8bddbed853c806b90b52f0f4e1a6db02bf580921
VictorMacintosh <semenchuk.vitk@gmail.com>
2016-10-12 12:29:24 +0300

    Add LoginView and ConversationTableViewController for iPhone

--- FlamingoAppMac/FlamingoAppMac/LoginWindowController.swift
+++ FlamingoAppMac/FlamingoAppMac/LoginWindowController.swift
@@ -13,15 +13,17 @@ class LoginWindowController: NSWindowController {
         super.windowDidLoad()
         
         
-        let view = NSVisualEffectView(frame: NSRect(x: 0, y: 0, width: 1500, height: 800))
+        /*let view = NSVisualEffectView(frame: NSRect(x: 0, y: 0, width: 1500, height: 800))
         view.blendingMode = NSVisualEffectBlendingMode.behindWindow
         view.material = NSVisualEffectMaterial.ultraDark
         view.state = NSVisualEffectState.active
-        self.window!.contentView!.addSubview(view)
+        self.window!.contentView!.addSubview(view)*/
+        
+        //window?.appearance = NSAppearance(named: "NSAppearanceNamedVibrantDark")
         
         
         self.window?.titleVisibility = NSWindowTitleVisibility.hidden;
-        self.window?.titlebarAppearsTransparent = true;
+        self.window?.titlebarAppearsTransparent = true
         self.window?.isOpaque = false
         self.window?.backgroundColor = NSColor(red: 50.0/255.0, green: 8.0/255.0, blue: 58.0/255.0, alpha: 0.95/1.0)
     
